b2d99c1b36c9e993bae3436c48052ef0e0a6ad9a
sonchaegeon <thscorjs1002@naver.com>
2021-07-01 13:52:29 +0900

    ⚡️ :: import profile module, provice app filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
+import { ProfileModule } from './profile/profile.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/exception/exception.filter';
 
 @Module({
-  imports: [TypeOrmConfigModule],
-  providers: [],
+  imports: [TypeOrmConfigModule, ProfileModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule {}
