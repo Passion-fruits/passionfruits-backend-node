d55e14afd13afc2d0a903fe0ff27b87d79cc6914
sonchaegeon <thscorjs1002@naver.com>
2021-07-03 22:04:59 +0900

    📑 :: modify profile dto

---
+++ src/profile/dto/modify-profile.dto.ts
@@ -0,0 +1,41 @@
+import { IsString, IsUrl, Length } from 'class-validator';
+
+export class ModifyProfileDto {
+  @IsString()
+  @Length(4, 30)
+  name: string;
+
+  @IsString()
+  @Length(0, 250)
+  bio: string;
+
+  @IsString()
+  @Length(0, 150)
+  @IsUrl()
+  insta: string;
+
+  @IsString()
+  @Length(0, 150)
+  @IsUrl()
+  facebook: string;
+
+  @IsString()
+  @Length(0, 150)
+  @IsUrl()
+  soundcloud: string;
+
+  @IsString()
+  @Length(0, 150)
+  @IsUrl()
+  youtube: string;
+}
+
+export class ModifyProfileResponseData {
+  name: string;
+  bio: string;
+  image_path: string;
+  insta: string;
+  facebook: string;
+  soundcloud: string;
+  youtube: string;
+}
