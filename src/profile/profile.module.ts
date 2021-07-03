63779666160351e9bd347eb15d06d881d9791531
sonchaegeon <thscorjs1002@naver.com>
2021-07-03 22:03:45 +0900

    ⚡️ :: profile module import sns repository

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfileRepository } from './entity/profile.repository';
+import { SnsRepository } from './entity/sns.repoistory';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProfileRepository])],
+  imports: [TypeOrmModule.forFeature([ProfileRepository, SnsRepository])],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
