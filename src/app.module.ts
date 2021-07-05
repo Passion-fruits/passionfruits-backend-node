1ec45095c4deb5e5fdb122314b9a1dea51b96fe4
sonchaegeon <thscorjs1002@naver.com>
2021-07-05 23:39:24 +0900

    ⚡️ :: import song module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { APP_FILTER } from '@nestjs/core';
 import { HttpErrorFilter } from './shared/exception/exception.filter';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './shared/jwt/passport/jwt.strategy';
+import { SongModule } from './song/song.module';
 
 @Module({
-  imports: [TypeOrmConfigModule, ProfileModule, JwtModule.register({})],
+  imports: [
+    TypeOrmConfigModule,
+    ProfileModule,
+    JwtModule.register({}),
+    SongModule,
+  ],
   providers: [
     JwtStrategy,
     {
