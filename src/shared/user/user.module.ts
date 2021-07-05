9accd7397f32b98c5d620a95088d484266ce9783
sonchaegeon <thscorjs1002@naver.com>
2021-07-05 23:44:25 +0900

    ⚰️ :: user module

--- src/shared/user/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from './entity/user.repository';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
-  providers: [],
-})
-export class UserModule {}
