258a9b7d5c36e517455dbce81d77d86ace6f3954
sonchaegeon <thscorjs1002@naver.com>
2021-07-12 09:05:54 +0900

    📑 :: get song response data

---
+++ src/song/dto/get-song.dto.ts
@@ -0,0 +1,13 @@
+export class GetSongResponseData {
+  song_id: number;
+  user_id: number;
+  cover_url: string;
+  song_url: string;
+  title: string;
+  description: string;
+  genre: number;
+  mood: number;
+  created_at: Date;
+  artist: string;
+  like: number;
+}
