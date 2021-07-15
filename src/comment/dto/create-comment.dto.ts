8f95755eee3043a4568ef6aa68a1ac5c8b41d7ef
sonchaegeon <thscorjs1002@naver.com>
2021-07-15 10:22:22 +0900

    📑 :: create comment dto

---
+++ src/comment/dto/create-comment.dto.ts
@@ -0,0 +1,10 @@
+import { IsNumber, IsString, Length } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsNumber()
+  song_id: number;
+
+  @IsString()
+  @Length(1, 200)
+  comment: string;
+}
