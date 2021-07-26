8a542686f2a18aec3028b48d5eb358c24e84881d
sonchaegeon <thscorjs1002@naver.com>
2021-07-26 11:10:52 +0900

    ⚡️ :: (#18) import user repository

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from 'src/shared/entity/user/user.repository';
 import { ProfileRepository } from './entity/profile.repository';
 import { SnsRepository } from './entity/sns.repoistory';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProfileRepository, SnsRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      ProfileRepository,
+      SnsRepository,
+      UserRepository,
+    ]),
+  ],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
