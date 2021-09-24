325a2d30bfdbe5d5c7c72278c8e9771d83139fa7
sonchaegeon <thscorjs1002@naver.com>
2021-09-24 19:10:28 +0900

    ♻️ :: (#50) snake case

--- src/playlist/entity/playlist-has-song.entity.ts
+++ src/playlist/entity/playlist-has-song.entity.ts
@@ -5,12 +5,12 @@ import { Song } from 'src/song/entity/song.entity';
 @Entity('playlist_has_song')
 export class PlaylistHasSong {
   @PrimaryColumn()
-  @ManyToOne(() => Playlist, (playlist) => playlist.playlistHasSong)
+  @ManyToOne(() => Playlist, (playlist) => playlist.play_list_has_song)
   @JoinColumn({ name: 'playlist_id' })
   playlist_id: number;
 
   @PrimaryColumn()
-  @ManyToOne(() => Song, (song) => song.playlistHasSong)
+  @ManyToOne(() => Song, (song) => song.playlist_has_song)
   @JoinColumn({ name: 'song_id' })
   song_id: number;
 }
