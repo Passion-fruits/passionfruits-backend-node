2679f5baaefbd3923da7d1942ba6c262104f739f
sonchaegeon <thscorjs1002@naver.com>
2021-09-24 16:38:33 +0900

    ⚡️ :: (#52) delete song in playlist service

--- src/playlist/playlist.service.ts
+++ src/playlist/playlist.service.ts
@@ -3,11 +3,13 @@ import { REQUEST } from '@nestjs/core';
 import { InjectRepository } from '@nestjs/typeorm';
 import {
   NotFoundPlaylistException,
+  NotFoundPlaylistHasSongException,
   PlaylistHasSongExistException,
 } from 'src/shared/exception/exception.index';
 import { IUserReqeust } from 'src/shared/interface/request.interface';
 import { AddSongInPlaylistDto } from './dto/add-song-in-playlist.dto';
 import { CreatePlaylistDto } from './dto/create-playlist.dto';
+import { DeleteSongInPlaylistDto } from './dto/delete-song-in-playlist.dto';
 import { PlaylistHasSongRepository } from './entity/playlist-has-song.repository';
 import { PlaylistRepository } from './entity/playlist.repository';
 
@@ -38,9 +40,29 @@ export class PlaylistService {
       song_id: dto.song_id,
       playlist_id,
     });
-    console.log(playlistHasSongRecord);
     if (playlistHasSongRecord) throw PlaylistHasSongExistException;
 
     await this.playlistHasSongRepository.addSongInPlaylist(dto, playlist_id);
+  }
+
+  public async deleteSongInPlaylist(
+    dto: DeleteSongInPlaylistDto,
+    playlist_id: number,
+  ): Promise<void> {
+    const playlistRecord = await this.playlistRepository.findOne({
+      id: playlist_id,
+      user: this.request.user.sub,
+    });
+    if (!playlistRecord) throw NotFoundPlaylistException;
+    const playlistHasSongRecord = await this.playlistHasSongRepository.findOne({
+      song_id: dto.song_id,
+      playlist_id,
+    });
+    if (!playlistHasSongRecord) throw NotFoundPlaylistHasSongException;
+
+    await this.playlistHasSongRepository.delete({
+      song_id: dto.song_id,
+      playlist_id,
+    });
   }
 }
