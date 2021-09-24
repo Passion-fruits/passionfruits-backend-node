96a590a3812f92b9c232a1616d865703bd0d79d7
sonchaegeon <thscorjs1002@naver.com>
2021-09-24 16:29:27 +0900

    ⚡️ :: (#51) import playlist has song repository

--- src/playlist/playlist.module.ts
+++ src/playlist/playlist.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlaylistHasSongRepository } from './entity/playlist-has-song.repository';
 import { PlaylistRepository } from './entity/playlist.repository';
 import { PlaylistController } from './playlist.controller';
 import { PlaylistService } from './playlist.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PlaylistRepository])],
+  imports: [
+    TypeOrmModule.forFeature([PlaylistRepository, PlaylistHasSongRepository]),
+  ],
   controllers: [PlaylistController],
   providers: [PlaylistService],
 })
