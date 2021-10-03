eec0362693fe9b7c02710dd9e04c3b12c39c4f0d
sonchaegeon <thscorjs1002@naver.com>
2021-10-03 20:09:34 +0900

    ⚡️ :: proxy remote address ip header

--- src/config/logger/logger.middleware.ts
+++ src/config/logger/logger.middleware.ts
@@ -15,7 +15,9 @@ export class LoggerMiddleware implements NestMiddleware {
     const _url = JSON.stringify(tempUrl ? tempUrl : {});
     const _auth = JSON.stringify(tempAuth ? tempAuth.split(' ')[1] : {});
 
-    loggerService.log(`${req.ip} ${_url} ${_query} ${_body} ${_auth} `);
+    loggerService.log(
+      `${req.headers['X-Real-IP']} ${_url} ${_query} ${_body} ${_auth}`,
+    );
     next();
   }
 }
