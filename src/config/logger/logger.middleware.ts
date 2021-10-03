bf91f4c08ca90669282a0cc634d801723115cb81
sonchaegeon <thscorjs1002@naver.com>
2021-10-03 19:46:34 +0900

    🐛 :: split undefined bug

--- src/config/logger/logger.middleware.ts
+++ src/config/logger/logger.middleware.ts
@@ -9,15 +9,13 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     const loggerService = new LoggerService();
     const tempUrl = req.method + ' ' + req.baseUrl;
+    const tempAuth = req.headers['authorization'].split(' ')[1];
     const _query = JSON.stringify(req.query ? req.query : {});
     const _body = JSON.stringify(req.body ? req.body : {});
     const _url = JSON.stringify(tempUrl ? tempUrl : {});
+    const _auth = JSON.stringify(tempAuth ? tempAuth : {});
 
-    loggerService.log(
-      `${req.ip} ${_url} ${_query} ${_body} ${
-        req.headers['authorization'].split(' ')[1]
-      } `,
-    );
+    loggerService.log(`${req.ip} ${_url} ${_query} ${_body} ${_auth} `);
     next();
   }
 }
