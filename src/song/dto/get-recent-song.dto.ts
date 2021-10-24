7a111d98424b5d449cb01680442e4c5a2af0fa7d
sonchaegeon <thscorjs1002@naver.com>
2021-10-24 12:17:48 +0900

    ⚡️ :: (#81) get recent song dto

---
+++ src/song/dto/get-recent-song.dto.ts
@@ -0,0 +1,15 @@
+export class GetRecentSongResponseData {
+  max_song: number;
+  song: RecentSongVo[];
+}
+
+export class RecentSongVo {
+  song_id: number;
+  user_id: number;
+  cover_url: string;
+  song_url: string;
+  title: string;
+  genre: string;
+  artist: string;
+  like: number;
+}
