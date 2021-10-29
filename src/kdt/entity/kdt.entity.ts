088ab8efac0aa6af7f3f5a5979881f6204c7186b
sonchaegeon <thscorjs1002@naver.com>
2021-10-29 22:09:44 +0900

    ⚡️ :: (#92) kdt entity

---
+++ src/kdt/entity/kdt.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
+import { User } from '../../shared/entity/user/user.entity';
+
+@Entity('kdt')
+export class Kdt {
+  @PrimaryColumn()
+  @OneToOne(() => User, (user) => user.kdt)
+  @JoinColumn({ name: 'user_id' })
+  user_id: number;
+
+  @Column({ type: 'bigint', default: 0 })
+  add_kdt: number;
+
+  @Column({ type: 'bigint', default: 0 })
+  donate_kdt: number;
+}
