d861d3278040b17a4cd53c731f7b8a8ac8a009a1
sonchaegeon <thscorjs1002@naver.com>
2021-10-29 22:10:03 +0900

    ⚡️ :: (#92) kdt repository success payment query

---
+++ src/kdt/entity/kdt.repository.ts
@@ -0,0 +1,14 @@
+import { User } from '../../shared/entity/user/user.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { Kdt } from './kdt.entity';
+
+@EntityRepository(Kdt)
+export class KdtRepository extends Repository<Kdt> {
+  public async accessPayment(
+    kdtAmount: number,
+    user_id: number,
+  ): Promise<void> {
+    const kdtRecord = await this.findOne({ user_id });
+    await this.update(kdtRecord, { add_kdt: kdtRecord.add_kdt + kdtAmount });
+  }
+}
