e0c050cd482d5cfbf12ab0c91e23b8cdb6333cb3
sonchaegeon <thscorjs1002@naver.com>
2021-10-30 11:10:45 +0900

    ⚡️ :: (#103) get historys controller

--- src/history/history.controller.ts
+++ src/history/history.controller.ts
@@ -1,12 +1,15 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GetHistoryResponseData } from './dto/get-historys.dto';
 import { HistoryService } from './history.service';
 
 @Controller('history')
@@ -18,5 +21,14 @@ export class HistoryController {
   @Post()
   public async recordHistory(@Body('song_id') song_id: number): Promise<void> {
     await this.historyService.recordHistory(song_id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  public getHistorys(
+    @Query('page') page: number,
+    @Query('size') size: number,
+  ): Promise<GetHistoryResponseData> {
+    return this.historyService.getHistorys(page, size);
   }
 }
