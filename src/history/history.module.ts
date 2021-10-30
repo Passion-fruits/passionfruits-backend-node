30661fe23640be25fcf23b3a656cb7fd300820d8
sonchaegeon <thscorjs1002@naver.com>
2021-10-30 11:10:52 +0900

    ⚡️ :: (#103) import history view repository

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SongRepository } from 'src/song/entity/song.repository';
+import { HistoryViewRepository } from './entity/history-view/history-view.repository';
 import { HistoryRepository } from './entity/history.repository';
 import { HistoryController } from './history.controller';
 import { HistoryService } from './history.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HistoryRepository, SongRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      HistoryRepository,
+      SongRepository,
+      HistoryViewRepository,
+    ]),
+  ],
   controllers: [HistoryController],
   providers: [HistoryService],
 })
