c787997c9c0b0b0e1c4bc94098d8e35ce254fa5c
sonchaegeon <thscorjs1002@naver.com>
2021-10-30 17:55:38 +0900

    ⚡️ :: (#110) kdt history repository get kdt history query

--- src/kdt/entity/kdt-history.repository.ts
+++ src/kdt/entity/kdt-history.repository.ts
@@ -1,4 +1,8 @@
 import { EntityRepository, Repository } from 'typeorm';
+import {
+  GetKdtHistoryResponseData,
+  KdtHistoryVo,
+} from '../dto/get-kdt-history.dto';
 import { KdtHistory } from './kdt-history.entity';
 
 @EntityRepository(KdtHistory)
@@ -18,5 +22,16 @@ export class KdtHistoryRepository extends Repository<KdtHistory> {
       kdt_type: 1,
     });
     await this.save(newKdtHistory);
+  }
+
+  public async getKdtHistory(user_id: number): Promise<KdtHistoryVo[]> {
+    return this.createQueryBuilder('hs')
+      .select('hs.order_id', 'order_id')
+      .addSelect('hs.created_at', 'created_at')
+      .addSelect('hs.amount', 'amount')
+      .addSelect('hs.tx_hash', 'tx_hash')
+      .addSelect('hs.kdt_type', 'kdt_type')
+      .where('hs.user_id = :user_id', { user_id })
+      .getRawMany();
   }
 }
