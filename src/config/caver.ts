646c884e43dd55ceefa6f7b21b207acd48940dfe
sonchaegeon <thscorjs1002@naver.com>
2021-10-30 16:58:54 +0900

    ⚡️ :: (#106) caver config

---
+++ src/config/caver.ts
@@ -0,0 +1,10 @@
+const Caver = require('caver-js');
+const caver = new Caver(process.env.BAOBAB_TESTNET_HOST);
+
+const keyring = new caver.wallet.keyring.singleKeyring(
+  process.env.FEE_PAYER_ADDRESS,
+  process.env.FEE_PAYER_SECRET,
+);
+caver.wallet.add(keyring);
+
+export const KIP7 = new caver.kct.kip7(process.env.KDT_CONTRACT_ADDRESS);
