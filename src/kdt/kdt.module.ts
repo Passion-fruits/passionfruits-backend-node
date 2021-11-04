59980bc6678ee29b6e04fd3e00ad00a249a9b662
sonchaegeon <thscorjs1002@naver.com>
2021-11-04 09:02:15 +0900

    ⚡️ :: (#120) import profile repository, user repository, message repository

--- src/kdt/kdt.module.ts
+++ src/kdt/kdt.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { KdtHistoryRepository } from './entity/kdt-history.repository';
-import { KdtRepository } from './entity/kdt.repository';
+import { ProfileRepository } from 'src/profile/entity/profile.repository';
+import { UserRepository } from 'src/shared/entity/user/user.repository';
+import { KdtHistoryRepository } from './entity/kdt-history/kdt-history.repository';
+import { KdtRepository } from './entity/kdt/kdt.repository';
+import { MessageRepository } from './entity/message/message.repository';
 import { KdtController } from './kdt.controller';
 import { KdtService } from './kdt.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([KdtRepository, KdtHistoryRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      KdtRepository,
+      KdtHistoryRepository,
+      ProfileRepository,
+      UserRepository,
+      MessageRepository,
+    ]),
+  ],
   controllers: [KdtController],
   providers: [KdtService],
 })
