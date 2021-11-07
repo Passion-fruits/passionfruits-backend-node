1385e5e46fc9f8c46d4d2a201a6e83eac51600ef
sonchaegeon <thscorjs1002@naver.com>
2021-11-07 17:53:02 +0900

    ⚡️ :: add is optional decorator

--- src/profile/dto/modify-profile.dto.ts
+++ src/profile/dto/modify-profile.dto.ts
@@ -1,26 +1,31 @@
-import { IsString, IsUrl, Length } from 'class-validator';
+import { IsOptional, IsString, IsUrl, Length } from 'class-validator';
 
 export class ModifyProfileDto {
   @IsString()
   @Length(2, 30)
   name: string;
 
+  @IsOptional()
   @IsString()
   @Length(0, 250)
   bio: string;
 
+  @IsOptional()
   @IsString()
   @Length(0, 150)
   insta: string;
 
+  @IsOptional()
   @IsString()
   @Length(0, 150)
   facebook: string;
 
+  @IsOptional()
   @IsString()
   @Length(0, 150)
   soundcloud: string;
 
+  @IsOptional()
   @IsString()
   @Length(0, 150)
   youtube: string;
